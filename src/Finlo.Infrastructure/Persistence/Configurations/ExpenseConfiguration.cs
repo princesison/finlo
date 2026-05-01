950a6b78b4106d70163c39eb593197b10626362a
princesison <princesison305@gmail.com>
2026-05-01 19:30:44 +0800

    Add project references and configure DbContext for Expense tracking

---
+++ src/Finlo.Infrastructure/Persistence/Configurations/ExpenseConfiguration.cs
@@ -0,0 +1,19 @@
+using Finlo.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finlo.Infrastructure.Persistence.Configurations;
+
+public class ExpenseConfiguration : IEntityTypeConfiguration<Expense>
+{
+    public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Expense> builder)
+    {
+        builder.HasKey(e => e.Id);
+
+        builder.Property(e => e.Amount)
+            .IsRequired()
+            .HasColumnType("decimal(18,2)");
+
+        builder.Property(e => e.Description)
+            .HasMaxLength(500);
+    }
+}
---
+++ src/Finlo.Infrastructure/Persistence/FinloDbContext.cs
@@ -0,0 +1,23 @@
+using System.Reflection;
+using Finlo.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finlo.Infrastructure.Persistence;
+
+public class FinloDbContext : DbContext
+{
+    public FinloDbContext(DbContextOptions<FinloDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Expense> Expenses { get; set; }
+    public DbSet<Income> Incomes { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
